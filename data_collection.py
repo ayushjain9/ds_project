573c77209b7f24a3e4b3637e90cdbe4877e9aee5
ayushjain9 <ayushjain9@deloitte.com>
2020-06-14 16:32:48 +0530

    uploaded scraper and run code

---
+++ data_collection.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 14 14:43:10 2020
+
+@author: ayushjain
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "C:/Users/ayushjain9/Documents/Projects/ds_project/chromedriver.exe"
+
+df = gs.get_jobs('data scientist',5, False, path, 15) # for 5 records
+
+df
